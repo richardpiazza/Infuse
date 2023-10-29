293108cab13da7056e643d3e3df01a528a661710
Richard Piazza <richard@richardpiazza.com>
2023-10-29 17:14:18 -0500

    Reset Swift Tools to match GitHub Runners

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Infuse",
+    platforms: [
+        .macOS(.v12),
+        .macCatalyst(.v15),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
